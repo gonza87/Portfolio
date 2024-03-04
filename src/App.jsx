996f4c2956ab3624bce3eb2a15e05074fc81ac79
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-03-04 12:12:17 -0300

    mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
           <p className="fs-2">Mis Proyectos</p>
           <div className="d-flex flex-wrap justify-content-between ">
             <Card className="cardProyectos" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/img/Untitled.gif" />
+              <Card.Img variant="top" src="/gif/HackShop.gif" />
               <Card.Body>
                 <Card.Title className="titleCardProyect">HackShop</Card.Title>
                 <Card.Text className="textCardProyect">
@@ -79,7 +79,7 @@ function App() {
               </Card.Body>
             </Card>
             <Card className="cardProyectos" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/img/hackflix.JPG" />
+              <Card.Img variant="top" src="/gif/Hackflix.gif" />
               <Card.Body>
                 <Card.Title className="titleCardProyect">HackFlix</Card.Title>
                 <Card.Text className="textCardProyect">
@@ -97,7 +97,7 @@ function App() {
               </Card.Body>
             </Card>
             <Card className="cardProyectos" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/img/tweter.JPG" />
+              <Card.Img variant="top" src="/gif/twitter.gif" />
               <Card.Body>
                 <Card.Title className="titleCardProyect">Twitter</Card.Title>
                 <Card.Text className="textCardProyect">
@@ -115,7 +115,7 @@ function App() {
               </Card.Body>
             </Card>
             <Card className="cardProyectos" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/img/todosurfjs.JPG" />
+              <Card.Img variant="top" src="/gif/TodoSurfJs.gif" />
               <Card.Body>
                 <Card.Title className="titleCardProyect">TodoSurf</Card.Title>
                 <Card.Text className="textCardProyect">
@@ -133,7 +133,7 @@ function App() {
               </Card.Body>
             </Card>
             <Card className="cardProyectos" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/img/todosurf.JPG" />
+              <Card.Img variant="top" src="/gif/Todosurf.gif" />
               <Card.Body>
                 <Card.Title className="titleCardProyect">TodoSurf</Card.Title>
                 <Card.Text className="textCardProyect">
@@ -168,6 +168,14 @@ function App() {
                 </Card.Link>
               </Card.Body>
             </Card>
+            <Card
+              className="cardProyectosMas"
+              style={{ width: "18rem" }}
+            ></Card>
+            <Card
+              className="cardProyectosMas"
+              style={{ width: "18rem" }}
+            ></Card>
           </div>
 
           {/* <div className="d-flex flex-wrap justify-content-between ">
